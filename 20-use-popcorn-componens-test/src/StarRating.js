7fcc24ad3f5928e60f7ee7fe31c819d96e1b0b45
jeromos55 <jeromos55@gmail.com>
2023-11-01 20:44:53 +0100

    update react component

--- 20-use-popcorn-componens-test/src/StarRating.js
+++ 20-use-popcorn-componens-test/src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -10,15 +11,24 @@ const starContainerStyle = {
   display: "flex",
 };
 
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
+};
+
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
   size = 36,
   className = "",
-  desfaultRating = 0,
+  defaultRating = 0,
   onSetRating,
 }) {
-  const [rating, setRating] = useState(desfaultRating);
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
